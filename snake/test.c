e537a62b00a53cbbb4479cd1eeff95bedd8c4645
Tdaine <1916686509@qq.com>
2019-03-19 23:56:10 +0800

    贪吃蛇
    简单实现贪吃蛇小游戏
    实现通过上下左右控制蛇的前行，实现变速、积分等功能

---
+++ snake/test.c
@@ -0,0 +1,18 @@
+﻿#include"snake.h"
+
+void test()
+{
+	Snake snake = { 0 };
+	srand((unsigned)time(NULL));
+	GameStart(&snake);
+	GameRun(&snake);
+	GameEnd(&snake);
+}
+
+int main()
+{
+	test();
+	SetPos(20, 20);
+	system("pause");
+	return 0;
+}
