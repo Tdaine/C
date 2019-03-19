c89739b56751366984d130445a953f6b29837912
Tdaine <1916686509@qq.com>
2019-03-19 23:48:47 +0800

    贪吃蛇
    简单贪吃蛇，使用上下左右控制，实现加速，暂停，积分等基本操作

--- test.c
+++ test.c
@@ -1,64 +1,18 @@
-﻿
-#include"DoCommuntConvert.h"
+﻿#include"snake.h"
 
-const char *search = "C:\\Users\\dell\\Desktop\\bit";
-
-void test(const char * search)
+void test()
 {
-	char add[200];
-	char add2[200];
-	strcpy(add, search);
-	strcpy(add2, search);
-	strcat(add2, "c++");
-	strcat(add, "\\*.c");
-	mkdir(add2);
-	long handle;
-	struct _finddata_t fileinfo;
-	handle = _findfirst(add, &fileinfo);
-	if (-1 == handle)
-		return 0;
-	do
-	{
-			strcpy(add, search);
-			strcpy(add2, search);
-			strcat(add, "\\");
-			strcat(add2, "c++\\");
-			strcat(add, fileinfo.name);
-			strcat(add2, fileinfo.name);
-			test(add);
-			FILE *PfIn = NULL;
-			FILE *PfOut = NULL;
-			PfIn = fopen(add, "r");
-			PfOut = fopen(add2, "w");
-			if (PfIn == NULL)
-			{
-				perror("read file:");
-				fclose(PfIn);
-				exit(EXIT_FAILURE);
-			}
-			if (PfOut == NULL)
-			{
-				perror("write file:");
-				fclose(PfIn);
-				exit(EXIT_FAILURE);
-			}
-			DoCommentConvert(PfIn, PfOut);
-			fclose(PfIn);
-			PfIn = NULL;
-			fclose(PfOut);
-			PfOut = NULL;
-		
-	}while (!_findnext(handle, &fileinfo));
-	_findclose(handle);
-	printf("转换成功\n");
+	Snake snake = { 0 };
+	srand((unsigned)time(NULL));
+	GameStart(&snake);
+	GameRun(&snake);
+	GameEnd(&snake);
 }
-
-
 
 int main()
 {
-	test(search);
+	test();
+	SetPos(20, 20);
 	system("pause");
 	return 0;
 }
-
